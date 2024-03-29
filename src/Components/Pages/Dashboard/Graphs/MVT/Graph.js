fe6939a7ceaf77f8cacde442185d6d7b8ed898b8
aunshx <aunsh.sb@gmail.com>
2024-03-29 01:51:53 -0700

    Partial changes to the Mvt graph UI

--- src/Components/Pages/Dashboard/Graphs/MVT/Graph.js
+++ src/Components/Pages/Dashboard/Graphs/MVT/Graph.js
@@ -73,9 +73,120 @@ const MvtGraph = ({ points }) => {
                             },
                         ],
                     },
-                ]}
+                ]
+            }
+            theme={theme}
         />
     );
+}
+
+let theme = {
+    "text": {
+        "fontSize": 12,
+        "fill": "#333333",
+        "outlineWidth": 0,
+        "outlineColor": "transparent"
+    },
+    "axis": {
+        "domain": {
+            "line": {
+                "stroke": "#777777",
+                "strokeWidth": 1
+            }
+        },
+        "legend": {
+            "text": {
+                "fontSize": 15,
+                "fill": "#c4c4c4",
+                "outlineWidth": 3,
+                "outlineColor": "transparent",
+            }
+        },
+        "ticks": {
+            "line": {
+                "stroke": "#777777",
+                "strokeWidth": 1
+            },
+            "text": {
+                "fontSize": 13,
+                "fill": "#c4c4c4",
+                "outlineWidth": 0,
+                "outlineColor": "transparent"
+            }
+        }
+    },
+    "grid": {
+        "line": {
+            "stroke": "#c4c4c4",
+            "strokeWidth": 0.5
+        }
+    },
+    "legends": {
+        "title": {
+            "text": {
+                "fontSize": 13,
+                "fill": "#c4c4c4",
+                "outlineWidth": 0,
+                "outlineColor": "transparent"
+            }
+        },
+        "text": {
+            "fontSize": 13,
+            "fill": "#c4c4c4",
+            "outlineWidth": 0,
+            "outlineColor": "transparent"
+        },
+        "ticks": {
+            "line": {},
+            "text": {
+                "fontSize": 10,
+                "fill": "#333333",
+                "outlineWidth": 0,
+                "outlineColor": "transparent"
+            }
+        }
+    },
+    "annotations": {
+        "text": {
+            "fontSize": 13,
+            "fill": "#333333",
+            "outlineWidth": 2,
+            "outlineColor": "#ffffff",
+            "outlineOpacity": 1
+        },
+        "link": {
+            "stroke": "#000000",
+            "strokeWidth": 1,
+            "outlineWidth": 2,
+            "outlineColor": "#ffffff",
+            "outlineOpacity": 1
+        },
+        "outline": {
+            "stroke": "#000000",
+            "strokeWidth": 2,
+            "outlineWidth": 2,
+            "outlineColor": "#ffffff",
+            "outlineOpacity": 1
+        },
+        "symbol": {
+            "fill": "#000000",
+            "outlineWidth": 2,
+            "outlineColor": "#ffffff",
+            "outlineOpacity": 1
+        }
+    },
+    "tooltip": {
+        "container": {
+            "background": "#ffffff",
+            "color": "#333333",
+            "fontSize": 12
+        },
+        "basic": {},
+        "chip": {},
+        "table": {},
+        "tableCell": {},
+        "tableCellValue": {}
+    }
 }
 
 export default MvtGraph
