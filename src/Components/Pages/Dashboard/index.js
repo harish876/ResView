46f2503c5b87fc6d565cae7e05666ce4346e1630
aunshx <aunsh.sb@gmail.com>
2024-03-31 21:05:30 -0700

    Soc for Mvt

--- src/Components/Pages/Dashboard/index.js
+++ src/Components/Pages/Dashboard/index.js
@@ -2,20 +2,19 @@
 
 
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from "react";
-import { GraphResizerContext, GraphViewContext } from "../../../Context/graph";
+import React, { useEffect, useState } from "react";
 import { anglesRightIcon, eyeIcon } from "../../../Resources/Icons";
+import { WebSocketDemo } from '../../../Socket';
 import { LinkButton } from '../../Shared/Buttons';
+import HRline from '../../Shared/HRline';
 import { Icon } from '../../Shared/Icon';
 import Title, { FontVarTitle, Subtitle } from '../../Shared/Title';
 import Wrapper from "../../Shared/Wrapper";
 import Input from '../Visualizer/Components/Input';
 import Mvt from './Graphs/MVT';
 import PBFT from './Graphs/PBFT';
-import HRline from '../../Shared/HRline';
 import ResizableContainer from './ResizableContainer';
-import TransInfo from './TransInfo'
-import { WebSocketDemo } from '../../../Socket';
+import TransInfo from './TransInfo';
 
 
 const colorList = ["hsl(148, 70%, 50%)", "hsl(200, 70%, 50%)", "hsl(171, 70%, 50%)", "hsl(313, 70%, 50%)"];
@@ -24,16 +23,8 @@ const colorList = ["hsl(148, 70%, 50%)", "hsl(200, 70%, 50%)", "hsl(171, 70%, 50
 
 
 const Dashboard = () => {
-    const { graph, mvtGraphNo } = useContext(GraphViewContext);
-    const { boxValues, setBoxValues, setResizing } =
-        useContext(GraphResizerContext);
-
-
     const [messageHistory, setMessageHistory] = useState({});
     const [currentTransaction, setCurrentTransaction] = useState(0);
-    const [messageChartData, setMessageChartData] = useState([]);
-    const [labelToggle, setLabelToggle] = useState({ "Replica 1": true, "Replica 2": true, "Replica 3": true, "Replica 4": true });
-    const [resetGraph, setResetGraph] = useState(0);
     const [replicaStatus, setReplicaStatus] = useState([false, false, false, false])
 
 
@@ -84,109 +75,6 @@ const Dashboard = () => {
     
         return () => clearInterval(interval);
       }, []);
-
-
-
-
-    useEffect(() => {
-        if (!(currentTransaction in messageHistory) || messageHistory[currentTransaction].current_time < 0) {
-            setMessageChartData([[], []])
-            // console.log(currentTransaction, " Not in messageHistory")
-        }
-        else {
-            const transactionData = messageHistory[currentTransaction];
-            console.log("TRANSACTION", transactionData)
-            let startTime = 0;
-            let firstPrepareTime = 0;
-            let pre_prepare_times = [];
-            let prepare_times = [];
-            let all_prepare_times = [];
-            let all_commit_times = [];
-            let label_list = [];
-
-
-            Object.keys(transactionData).forEach((key) => {
-                label_list.push("Replica " + key);
-                if (transactionData[key].primary_id !== transactionData[key].replica_id) {
-                    pre_prepare_times.push(Math.floor(transactionData[key].propose_pre_prepare_time / 10000));
-                }
-                prepare_times.push(Math.floor(transactionData[key].prepare_time / 10000));
-                let replica_prepare_timestamps = [];
-                let replica_commit_timestamps = [];
-                transactionData[key]["prepare_message_timestamps"].map((time) => {
-                    replica_prepare_timestamps.push(Math.floor(time / 10000));
-                });
-                transactionData[key]["commit_message_timestamps"].map((time) => {
-                    replica_commit_timestamps.push(Math.floor(time / 10000));
-                });
-                all_prepare_times.push(replica_prepare_timestamps);
-                all_commit_times.push(replica_commit_timestamps);
-            });
-            startTime = Math.min(...pre_prepare_times);
-            firstPrepareTime = Math.min(...prepare_times);
-
-
-            let prepareChartData = [];
-            let commitChartData = [];
-            for (const element of all_prepare_times) {
-                let lineData = [{ x: 0, y: 0 }];
-                for (let j = 0; j < element.length; j++) {
-                    lineData.push({ x: element[j] - startTime, y: j });
-                    lineData.push({ x: element[j] - startTime, y: j + 1 });
-                }
-                prepareChartData.push(lineData);
-            }
-            for (const element of all_commit_times) {
-                let lineData = [{ x: 0, y: 0 }];
-                for (let j = 0; j < element.length; j++) {
-                    lineData.push({ x: element[j] - firstPrepareTime, y: j });
-                    lineData.push({ x: element[j] - firstPrepareTime, y: j + 1 });
-                }
-                commitChartData.push(lineData);
-            }
-
-
-            let preparePoints = [];
-            let data = {};
-            for (let i = 0; i < label_list.length; i++) {
-                if (!labelToggle[label_list[i]]) {
-                    data = {
-                        id: label_list[i],
-                        color: colorList[i],
-                        data: [],
-                    };
-                }
-                else {
-                    data = {
-                        id: label_list[i],
-                        color: colorList[i],
-                        data: prepareChartData[i],
-                    };
-                }
-                preparePoints.push(data);
-            }
-            let commitPoints = [];
-            for (let i = 0; i < label_list.length; i++) {
-                if (!labelToggle[label_list[i]]) {
-                    data = {
-                        id: label_list[i],
-                        color: colorList[i],
-                        data: [],
-                    };
-                }
-                else {
-                    data = {
-                        id: label_list[i],
-                        color: colorList[i],
-                        data: commitChartData[i],
-                    };
-                }
-                commitPoints.push(data);
-            }
-            let pointData = { 1: preparePoints, 2: commitPoints };
-            setMessageChartData(pointData);
-        }
-    }, [messageHistory, currentTransaction, labelToggle, resetGraph]);
 
 
     const sendGet = async (key) => {
