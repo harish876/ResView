48f552223942a1125c3ec30f27db47194fb8bef5
aunshx <aunsh.sb@gmail.com>
2024-05-26 12:00:28 -0700

    Links updated in Home and Navbar

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,7 +1,7 @@
-export const URL_HOME_PAGE = '/pages/home'
-export const URL_TEAM_PAGE = '/pages/team'
-export const URL_VISUALIZER_PAGE = '/pages/visualizer'
-export const URL_REROUTE_PAGE = '/pages/home'
+export const URL_HOME_PAGE = '/home'
+export const URL_TEAM_PAGE = '/team'
+export const URL_VISUALIZER_PAGE = '/visualizer'
+export const URL_REROUTE_PAGE = '/home'
 
 export const DEFAULT_IMAGE = 'https://i.postimg.cc/8PTkJPpQ/default-ui-image-placeholder-wireframes-600nw-1037719192-ezgif-com-webp-to-jpg-converter.jpg';
 
