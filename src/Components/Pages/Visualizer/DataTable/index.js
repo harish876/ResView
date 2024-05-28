2cf195aee9c308c0ca068b421e69127ad50a5589
aunshx <aunsh.sb@gmail.com>
2024-05-27 23:33:56 -0700

    Updates to table

--- src/Components/Pages/Visualizer/DataTable/index.js
+++ src/Components/Pages/Visualizer/DataTable/index.js
@@ -37,61 +37,62 @@ const CellValues = ({ value, loading, replicaDetailsKeys, replicaDetailsBool, pr
 const TableValues = ({ srNo, transaction, replicaDetailsKeys, loading }) => {
     return (
         <Fragment>
-            <tr className='border-b-1p'>
-                <CellValues 
-                    value={srNo} 
-                    loading={loading} 
-                    replicaDetailsKeys={replicaDetailsKeys} transaction={transaction} 
+            <tr className=''>
+                <CellValues
+                    value={srNo}
+                    loading={loading}
+                    replicaDetailsKeys={replicaDetailsKeys} transaction={transaction}
                     primaryDoesNotExist={transaction.primary}
                 />
                 {Object.keys(transaction).length > 0 && (
                     Object.keys(transaction).map((value, idx) => {
-                        if(value !== 'replicaDetails') return (
+                        if (value !== 'replicaDetails') return (
                             <CellValues
-                                value={transaction[value]} 
-                                loading={loading} 
-                                replicaDetailsKeys={replicaDetailsKeys} 
+                                value={transaction[value]}
+                                loading={loading}
+                                replicaDetailsKeys={replicaDetailsKeys}
                                 primaryDoesNotExist={transaction.primary}
                                 key={idx}
                             />
                         )
-                })
+                    })
                 )}
-            </tr>   
+            </tr>
             {replicaDetailsKeys.map((replicaKey, index) => {
                 const replica = transaction.replicaDetails[replicaKey];
                 return (
                     <tr key={replicaKey} className='border-b-1p'>
                         <CellValues
-                            value={replicaKey} 
-                            loading={loading} 
-                            replicaDetailsKeys={replicaDetailsKeys} 
-                            primaryDoesNotExist={transaction.primary}                            key={index}
+                            value={replicaKey}
+                            loading={loading}
+                            replicaDetailsKeys={replicaDetailsKeys}
+                            primaryDoesNotExist={transaction.primary}
+                            key={index}
                             replicaDetailsBool
                         />
                         {Object.keys(replica).length > 0 && (
                             Object.keys(replica).map((value, idx) => (
                                 <CellValues
-                                    value={replica[value]} 
-                                    loading={loading} 
-                                    replicaDetailsKeys={replicaDetailsKeys} 
+                                    value={replica[value]}
+                                    loading={loading}
+                                    replicaDetailsKeys={replicaDetailsKeys}
                                     primaryDoesNotExist={transaction.primary}
                                     key={idx}
                                     replicaDetailsBool
                                 />
-                            )) 
+                            ))
                         )}
                     </tr>
                 );
-            })}         
+            })}
         </Fragment>
     );
 }
 
-const DataTable = ({ messageHistory, delay=3000 }) => {
+const DataTable = ({ messageHistory, delay = 3000 }) => {
 
     const [tableLoading, setTableLoading] = useState(false);
-    
+
     const [tableData, setTableData] = useState();
 
     useEffect(() => {
@@ -109,78 +110,78 @@ const DataTable = ({ messageHistory, delay=3000 }) => {
         return () => clearTimeout(timeoutId);
     }, [messageHistory, delay]);
 
-  return (
-      <div className="relative overflow-x-auto rounded-md border-3p bg-blue-10 dark:border-solid border-gray-700 dark:border-gray-50 h-550p">
-          <table className="w-full text-sm text-center rtl:text-right dark:text-gray-300 text-gray-700">
-              <thead className="text-xs uppercase dark:text-gray-300 text-gray-700 w-full border-b-1p border-gray-700 dark:border-gray-50">
-                  <tr>
-                      {TABLE_HEADERS[1].map((value, index) => {
-                          let isReplicaDetailCol = value === 'Replica Details' ? true : false;
-                          return (
-                              <th scope="col" className={classNames("px-6 py-3 border-r-1p border-gray-700 dark:border-gray-50", { 'border-b-1p border-r-0': isReplicaDetailCol })} rowSpan={!isReplicaDetailCol && '2'} colSpan={isReplicaDetailCol && '4'} key={index}>
-                                  {value}
-                              </th>
-                          )
-                      })}
-                  </tr>
-                  <tr>
-                      {TABLE_HEADERS[2].map((value, index) => {
-                          return (
-                              <th scope="col" className={classNames("px-6 py-3 border-gray-700 dark:border-gray-50", { 'border-r-1p': index !== TABLE_HEADERS[2].length - 1})} key={index}>
-                                  {value}
-                              </th>
-                          )
-                      })}
-                  </tr>
-              </thead>
-              <tbody>
-                  {tableLoading ? (
-                    <>
-                        {Object.keys(tableDataDummy).map((transactionKey, index) => {
-                        const transaction = tableDataDummy[transactionKey];
-                        const replicaDetailsKeys = Object.keys(transaction.replicaDetails);
-                        return (
-                            <TableValues
-                                className='cursor-pointer'
-                                key={transactionKey}
-                                srNo={index + 1}
-                                transaction={transaction} replicaDetailsKeys={replicaDetailsKeys}
-                                loading={tableLoading}
-                            />
-                        );
-                    })}
-                    </>
-                  ) : (
-                    <>
-                    {typeof tableData !== 'undefined' && Object.keys(tableData).map((transactionKey, index) => {
-                      const transaction = tableData[transactionKey];
-                      const replicaDetailsKeys = Object.keys(transaction.replicaDetails);
-                      return (
-                          <TableValues
-                              className='cursor-pointer'
-                              key={transactionKey}
-                              srNo={index + 1}
-                              transaction={transaction} replicaDetailsKeys={replicaDetailsKeys}
-                          />     
-                      );
-                  })}
-                    </>
-                  )}
-              </tbody>
-              
-          </table>
-          <div className="flex items-center justify-between px-8" aria-label="Table navigation">
-              <div>
-                  <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">20</span></span>
-              </div>
-              <div className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                  <div className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</div>
-                  <div className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</div>
-              </div>
-          </div>
-      </div>
-
-  )
+    return (
+        <div className="relative overflow-x-auto rounded-md border-3p bg-blue-10 dark:border-solid border-gray-700 dark:border-gray-50 h-600p">
+            <div className="overflow-y-auto h-550p scrollbar">
+                <table className="w-full text-sm text-center rtl:text-right dark:text-gray-300 text-gray-700">
+                    <thead className="text-xs uppercase dark:text-gray-300 text-gray-700 w-full border-gray-700 dark:border-gray-50 bg-gray-100 dark:bg-gray-800 sticky top-0 z-10">
+                        <tr>
+                            {TABLE_HEADERS[1].map((value, index) => {
+                                let isReplicaDetailCol = value === 'Replica Details' ? true : false;
+                                return (
+                                    <th scope="col" className={classNames("px-6 py-3 border-r-1p border-gray-700 dark:border-gray-50", { 'border-r-0': isReplicaDetailCol })} rowSpan={!isReplicaDetailCol && '2'} colSpan={isReplicaDetailCol && '4'} key={index}>
+                                        {value}
+                                    </th>
+                                )
+                            })}
+                        </tr>
+                        <tr>
+                            {TABLE_HEADERS[2].map((value, index) => {
+                                return (
+                                    <th scope="col" className={classNames("px-6 py-3 border-gray-700 dark:border-gray-50 border-t-1p border-r-1p")} key={index}>
+                                        {value}
+                                    </th>
+                                )
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableLoading ? (
+                            <>
+                                {Object.keys(tableDataDummy).map((transactionKey, index) => {
+                                    const transaction = tableDataDummy[transactionKey];
+                                    const replicaDetailsKeys = Object.keys(transaction.replicaDetails);
+                                    return (
+                                        <TableValues
+                                            className='cursor-pointer'
+                                            key={transactionKey}
+                                            srNo={index + 1}
+                                            transaction={transaction} replicaDetailsKeys={replicaDetailsKeys}
+                                            loading={tableLoading}
+                                        />
+                                    );
+                                })}
+                            </>
+                        ) : (
+                            <>
+                                {typeof tableData !== 'undefined' && Object.keys(tableData).map((transactionKey, index) => {
+                                    const transaction = tableData[transactionKey];
+                                    const replicaDetailsKeys = Object.keys(transaction.replicaDetails);
+                                    return (
+                                        <TableValues
+                                            className='cursor-pointer'
+                                            key={transactionKey}
+                                            srNo={index + 1}
+                                            transaction={transaction} replicaDetailsKeys={replicaDetailsKeys}
+                                        />
+                                    );
+                                })}
+                            </>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            <div className="flex items-center justify-between px-8 border-t-1p border-gray-700 dark:border-gray-50" aria-label="Table navigation">
+                <div>
+                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">20</span></span>
+                </div>
+                <div className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                    <div className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</div>
+                    <div className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default DataTable
+export default DataTable;
