7fae98b42491fa57e7b8dd8b3338ec44e25c2e31
aunshx <aunsh.sb@gmail.com>
2024-09-04 09:25:00 -0700

    V3-build

---
+++ build/static/js/8122.6983dde7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkresview=self.webpackChunkresview||[]).push([[8122],{5741:(e,n,t)=>{async function a(e){let n=!(arguments.length>1&&void 0!==arguments[1])||arguments[1];await e.addInteractor("externalBounce",(async e=>{const{Bouncer:n}=await t.e(7454).then(t.bind(t,7454));return new n(e)}),n)}t.d(n,{loadExternalBounceInteraction:()=>a})}}]);
+//# sourceMappingURL=8122.6983dde7.chunk.js.map
