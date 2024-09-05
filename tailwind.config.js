38a6773404994be63d2cab2c6ea68981e63ad0ab
aunshx <aunsh.sb@gmail.com>
2024-09-05 14:49:08 -0700

    Table and Throttling fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
