8d90957effbf2c23baf88f06a03ce017d68255b8
aunshx <aunsh.sb@gmail.com>
2024-09-16 10:03:15 -0700

    Removing dummy data when true data arrives

--- src/Context/visualizer.js
+++ src/Context/visualizer.js
@@ -118,6 +118,7 @@ export const VizDataHistoryProvider = ({ children }) => {
                 let port = parseInt(18501) + replicaPort;
                 const response = await fetch("http://localhost:" + String(port) + "/consensus_data");
                 const newData = await response.json();
+                if(newData) setMessageHistory({});
                 Object.keys(newData).map((key) => {
                     if (!keyList.current[replicaPort].includes(key)) {
                         keyList.current[replicaPort].push(key);
@@ -155,7 +156,7 @@ export const VizDataHistoryProvider = ({ children }) => {
         };
 
         updateStatus();
-        const interval = setInterval(updateStatus, 20000);
+        const interval = setInterval(updateStatus, 3000);
 
         return () => clearInterval(interval);
     }, []);
