ad33770bff6d5e5d1463429b7d91cfdfb5bc6283
aunshx <aunsh.sb@gmail.com>
2024-09-29 20:24:14 -0700

    favicon updated

--- build/static/js/3579.a968d629.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkresview=self.webpackChunkresview||[]).push([[3579],{3579:(e,t,r)=>{async function a(e){let t=!(arguments.length>1&&void 0!==arguments[1])||arguments[1];await e.addParticleUpdater("strokeColor",(async e=>{const{StrokeColorUpdater:t}=await r.e(231).then(r.bind(r,231));return new t(e)}),t)}r.d(t,{loadStrokeColorUpdater:()=>a})}}]);
-//# sourceMappingURL=3579.a968d629.chunk.js.map
