279a6526906cd9519cc0284eb31cf28d50f03753
aunshx <aunsh.sb@gmail.com>
2024-10-05 11:32:04 -0700

    Updated Build

---
+++ build/static/js/6341.cf5a72b3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkresview=self.webpackChunkresview||[]).push([[6341],{6341:(e,a,t)=>{async function n(e){let a=!(arguments.length>1&&void 0!==arguments[1])||arguments[1];const{StarDrawer:n}=await t.e(7604).then(t.bind(t,7604));await e.addShape("star",new n,a)}t.d(a,{loadStarShape:()=>n})}}]);
+//# sourceMappingURL=6341.cf5a72b3.chunk.js.map
